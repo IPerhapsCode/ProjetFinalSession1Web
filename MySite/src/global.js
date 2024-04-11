7d04b784d882332afb24d47d7636457fbb4b7468
IPerhapsCode <paulepaquin2002@gmail.com>
2024-04-10 23:08:30 -0400

    we going hard at it fr

--- MySite/src/global.js
+++ MySite/src/global.js
@@ -1,10 +1,37 @@
-introDelay = 0;
-slidePositionA = 0;
-slidePositionB = 100;
-momentum = 0.008;
-decreasingMomentum = 0.01192;
-speed = 0.0001;
-direction = "";
+// Import the THREE.js library
+import * as THREE from "three";
+// To allow for the camera to move around the scene
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// To allow for importing the .gltf file
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+//Variables
+let mouseX;
+let mouseY;
+
+//Intro variables
+let introDelay = 0;
+let slidePositionA = 0;
+let slidePositionB = 100;
+let momentum = 0.008;
+let decreasingMomentum = 0.01192;
+let speed = 0.0001;
+let direction = "";
+let zoneInit;
+
+//Planet variables
+let zonePlanet;
+let clickingPlanet = false;
+let differenceX;
+let rotationSpeed = 0.5;
+const fullRotation = 6.275;
+let lastTime = Date.now();
+let lastMouseX = 0;
+let object;
+const scene = new THREE.Scene(); //To note
+const loader = new GLTFLoader();
+const renderer = new THREE.WebGLRenderer({alpha: true});
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 window.addEventListener("load", () => {
     zoneInit = document.querySelector("#zone-init");
@@ -13,14 +40,67 @@ window.addEventListener("load", () => {
     zonePlanet = document.querySelector("#zone-planet");
     // zonePlanet.scrollLeft = zonePlanet.scrollWidth/8; /*To note*/
     zonePlanet.style.background = "#212121";
+    
+    //To note 3D model implementation
+    loader.load(
+        "./media/model/low_poly_planet_earth.glb",
+        function(gltf){
+            object = gltf.scene;
+            let boundingBox = new THREE.Box3().setFromObject(object);
+            let center = new THREE.Vector3();
+            boundingBox.getCenter(center);
+            let offset = new THREE.Vector3();
+            offset.subVectors(object.position, center);
+            object.position.sub(offset);
+            scene.add(object);
+        },
+        function(xhr){
+            console.log("Loading: " + (xhr.loaded / xhr.total * 100) + "%")
+        },
+        function(error){
+            console.error(error);
+        }
+    );
+    //Renderer
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.querySelector("#planet").appendChild(renderer.domElement);
+    //Camera
+    camera.position.z = 5;
+    camera.position.y = 2.2;
+    //Light
+    const topLight = new THREE.DirectionalLight(0xffffff, 1);
+    topLight.position.set(500, 500, 500);
+    topLight.castShadow = true;
+    scene.add(topLight);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+    scene.add(ambientLight);
+
+    //Events
+    document.onmousemove = (e) =>{
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+    };
+
+    zonePlanet.addEventListener('mouseup', (e) =>{
+        clickingPlanet = false;
+    });
+
+    zonePlanet.addEventListener('mouseleave', (e) =>{
+        clickingPlanet = false;
+    });
+
+    zonePlanet.addEventListener('mousedown', (e) =>{
+        clickingPlanet = true;
+    });
 
     intro();
-})
+});
 
 const intro = () =>{
-    text = ["Hi", ",", " I'm", " Paul", "."];
-    counter = 0;
+    let text = ["Hi", ",", " I'm", " Paul", "."];
+    let counter = 0;
 
+    //Prints the first message
     print(document.querySelector("#a1-" + counter), text[counter++], 1000);
     print(document.querySelector("#a1-" + counter), text[counter++], 700);
     print(document.querySelector("#a1-" + counter), text[counter++], 1000);
@@ -30,9 +110,10 @@ const intro = () =>{
     setTimeout(() => {
         //On ne peut présentement pas avoir de paramètre dans la fonction slideOut
         direction = "-";
+        modelAnimation();
         slideOut();
     }, introDelay + 2000);
-}
+};
 
 const print = (node, text, time) =>{
     introDelay += time;
@@ -43,6 +124,7 @@ const print = (node, text, time) =>{
 };
 
 const slideOut = () =>{
+    //Change the momentum of our animation
     if(slidePositionB < 20)
     {
         speed -= decreasingMomentum;
@@ -50,16 +132,22 @@ const slideOut = () =>{
     else if(slidePositionB > 70){
         speed += momentum;
     }
+
+    //Add the speed value to the position of our objects
     slidePositionA += speed;
     slidePositionB -= speed;
 
+    //Makes sure the animation doesn't overshoot
     if(slidePositionB < 0)
     {
         slidePositionB = 0;
     }
 
+    //Change the position of animation
     zoneInit.style.top = direction + slidePositionA + "%";
     zonePlanet.style.top = slidePositionB + "%";
+
+    //Call this function once again if the animation is not over
     if(slidePositionB > 0)
     {
         window.requestAnimationFrame(slideOut);
@@ -68,8 +156,64 @@ const slideOut = () =>{
     {
         //Reset variables (Have to since I can't use parameters for this function)
         speed = 0.1;
-        slidePosition = 0;
+        slidePositionA = 0;
+        slidePositionB = 100;
         direction = "";
         zoneInit.remove();
     }
 };
+
+const modelAnimation = () =>{ //Could be interesting if the planet keeps momentum after being interacted with
+    let test = currentTopValue(zonePlanet);
+    if(test < 70 && test > -70)
+    {
+        //Standardisation of the animation
+        let now = Date.now();
+        if(clickingPlanet)
+        {
+            //Lets the user play with the model
+            differenceX = (mouseX - lastMouseX) / 750;
+            object.rotation.y += differenceX;
+        }
+        else
+        {
+            //The model continues spinning in the last direction the user pulled the model in
+            if(differenceX > 0 && Math.sign(rotationSpeed) == 1)
+            {
+                rotationSpeed = -rotationSpeed;
+            }
+            else if(differenceX <= 0 && Math.sign(rotationSpeed) == -1)
+            {
+                rotationSpeed = Math.abs(rotationSpeed);
+            }
+
+            //Spins the model
+            object.rotation.y -= rotationSpeed * (now - lastTime) / 1000;
+            object.rotation.y = object.rotation.y % fullRotation;
+        }
+        //Set variables for next call
+        lastTime = Date.now();
+        lastMouseX = mouseX;
+
+        //Renders the scene
+        renderer.render(scene, camera);    
+    }
+    window.requestAnimationFrame(modelAnimation);
+};
+
+//Extracts the value of the top variable of a node
+const currentTopValue = (node) =>{
+    let string = "";
+    for(let i = 0; i < node.style.top.length; ++i)
+    {
+        if(node.style.top[i] != '%')
+        {
+            string += node.style.top[i];
+        }
+        else
+        {
+            break;
+        }
+    }
+    return string;
+}
